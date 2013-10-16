45c7fb16d51a5c1d4189456da295139ad8caf928
John Le Drew <jp@antz29.com>
2013-10-16 10:58:55 +0100

    Update example.js

--- example.js
+++ example.js
@@ -2,13 +2,14 @@ var getArgs = require('./index')
 
 function ajax(url, opts, callback){
   var args = getArgs(
-    'url:string,[opts]:object,[callback]:function',
+    'url:string|array,[opts]:object,[callback]:function',
     arguments)
   console.log(JSON.stringify(args, null, '  '))
 }
 
 ajax()
 ajax('/submit')
+ajax(['/submit'])
 ajax('/submit', {method: 'POST', params: {foo: 'bar'}}, function(dat){
 
 })
